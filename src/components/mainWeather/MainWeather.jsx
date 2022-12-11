f16031bddf2945145da94e8c2f0465f0effba4a5
meta-ela <anxhelameta@live.it>
2022-12-11 18:55:16 +0100

    add css and getday function

--- src/components/mainWeather/MainWeather.jsx
+++ src/components/mainWeather/MainWeather.jsx
@@ -1,9 +1,12 @@
 import "./mainWeather.css";
 import "../../css/index.css";
 
+
+import {mapImage} from '../../utilities/WeatherUtil';
+
 function MainWeather(props) {
 
-    const getTodaysDate = () => {
+    /* const getTodaysDate = () => {
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -11,6 +14,10 @@ function MainWeather(props) {
 
         today = dd + '/' + mm + '/' + yyyy;
         return today;
+    } */
+
+    const getTodaysDateAsString = () => {
+        return new Date().toLocaleDateString('en-us', { weekday:"long", month:"long", day:"numeric"});
     }
 
     return (
@@ -21,13 +28,14 @@ function MainWeather(props) {
                         {props.data.name}
                     </h3>
                     <div className="fw-bold">
-                        {getTodaysDate()}
+                        {getTodaysDateAsString()}
                     </div>
                     <div>
                         {props.data.weather[0].main}
                     </div>
-                    <div className="tempContainer backColorGradient">
+                    <div className="tempContainer gap-3 backColorGradient">
                         {Math.round(props.data.main.temp)}°
+                        <img className='w-50' src={mapImage(props.data.weather[0].id)} alt="" />
                     </div>
                 </div>}
         </div>
